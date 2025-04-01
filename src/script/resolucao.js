64320fb07ef265e282227b6a70f3db4eb2cb1ed8
Yuri Souza <yuri.po.ca2018@gmail.com>
2025-04-01 20:22:42 -0300

    Delete src/script/resolucao.js

--- src/script/resolucao.js
+++
@@ -1,7 +0,0 @@
-function ajustarAltura() {
-    document.querySelector('.start').style.height = `${window.innerHeight}px`;
-  }
-  
-  ajustarAltura(); // Ajusta ao carregar a página
-  window.addEventListener('resize', ajustarAltura); // Reajusta no redimensionamento
-  
